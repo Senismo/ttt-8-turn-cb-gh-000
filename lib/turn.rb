0a8c72418e5371fa85b133d7c93e5f43da4f37bb
Salak Sen <benismail31@gmail.com>
2018-04-01 20:34:35 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,6 +6,14 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def input_to_index(input)
+  input.to_i - 1
+end
+
+def move(board, index, character="X")
+  board[index] = character
+end
+
 def valid_move?(board, index)
   if index.between?(0,8) && !position_taken?(board, index)
     return true
